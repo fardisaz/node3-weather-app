d31be5950358267a92acec342eea37049bb0cc9a
fardisaz <amoozadeh73@yahoo.com>
2022-03-13 17:51:30 +0100

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,15 +10,13 @@ weatherForm.addEventListener("submit", (e) => {
   console.log(location);
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather/?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          return (messageOne.textContent = data.error);
-        }
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forcast;
-      });
-    }
-  );
+  fetch(`/weather/?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        return (messageOne.textContent = data.error);
+      }
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forcast;
+    });
+  });
 });
